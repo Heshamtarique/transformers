57ca677d00b9537c0ff2299ebae28213d8ce954f
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-08-04 16:55:54 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -56,3 +56,19 @@ class ResidualConnection(nn.Module):
     
         def forward(self, x, sublayer):
             return x + self.dropout(sublayer(self.norm(x)))
+            
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model # Embedding vector size
+        self.h = h # Number of heads
+        # Make sure d_model is divisible by h
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model, bias=False) # Wq
+        self.w_k = nn.Linear(d_model, d_model, bias=False) # Wk
+        self.w_v = nn.Linear(d_model, d_model, bias=False) # Wv
+        self.w_o = nn.Linear(d_model, d_model, bias=False) # Wo
+        self.dropout = nn.Dropout(dropout)
