926ab7a173c9b08df2be54e39a83b415d6bc8fe8
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-09-01 23:37:31 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -17,3 +17,9 @@ def get_config():
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
+
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
