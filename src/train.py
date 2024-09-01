17b7e283b8562b0ccb8db1dbda43c2bd8896c479
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-09-01 23:35:43 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,19 @@
+from model import build_transformer
+from dataset import BilingualDataset, causal_mask
 
+import torchtext.datasets as datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
+
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
